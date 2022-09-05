b9554487924025b7a45de0ce7b926c60bb838af7
Sourayaak <sourayaabdelkader.gmail.com>
2022-09-05 08:00:16 +0300

    JS: adding an alert

--- game.js
+++ game.js
@@ -7,11 +7,12 @@ let stateCheck = setInterval(() => {
     const end = document.getElementById("end");
     const boundaries = document.getElementsByClassName("boundary");
     const status = document.getElementById("status");
-    let score = 0 ;
+    let score = 0;
 
     // functions
     function color(list) {
-      for (let i = 0; i< list.length; i++){
+      // length -1 : so the example won't be colored
+      for (let i = 0; i< list.length -1; i++){
         list[i].style.borderColor = "red"; };
     };
 
@@ -29,12 +30,13 @@ let stateCheck = setInterval(() => {
     
     // greeding user
     var name = prompt("Enter your name");  
-    var save = prompt("Do you want to save your score? (Y)es or (N)o").toLowerCase();
-    if (name != null) { alert("Welcome " + name);} ; 
+    if (name != null) { alert("Welcome " + name);};
+    var save = prompt("Do you want to save your score? (Y)es or (N)o").toLowerCase(); 
     
     // gets a score ; there is some scores stored in the local storage_
     if (localStorage.getItem(name) != null){
-      score = localStorage.getItem(name)
+      score = localStorage.getItem(name);
+      alert("You're previous score is "+ score);
     };
        
     // start the game 
@@ -44,8 +46,8 @@ let stateCheck = setInterval(() => {
       let boundary_counter = 0;
       let end_counter = 0;
       // counter is a variable set to make sure the end button and borders are hoovered once after hoovering the start 
-      start_counter +=1 ;
-      // colors borders when hoovered , with the example div
+      start_counter +=1;
+      // colors borders when hoovered 
       const boundaries1 = document.querySelectorAll(".boundary");
       boundaries1.forEach(boundary => {
         boundary.addEventListener("mouseover", mouseOver) ;
@@ -73,7 +75,8 @@ let stateCheck = setInterval(() => {
           end_counter = 0;
         };
       };
-      if (save == "Y") {
+      // will save just by entering "Y" or "y"
+      if (save == "y") {
         localStorage.setItem(name, score);
       }
     // refresh the page on start click
@@ -81,12 +84,4 @@ let stateCheck = setInterval(() => {
   };
   };
 }, 100);
-
-
-  
-
-
-
-
-
 
